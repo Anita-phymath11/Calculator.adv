dcee4a080118a9cacb6c3604ef4e45cb7f1bee57
Anita-phymath11 <a90k.karami@gmail.com>
2024-07-28 15:02:57 +0330

    Update Cal_Anita.py

--- Cal_Anita.py
+++ Cal_Anita.py
@@ -10,8 +10,8 @@ while b=="c":
         op1=input("enter an operator:")
         num2=int(input("enter another number:"))
     else:
-        num1=int(input("enter a number:"))
         op1=input("enter an operator:")
+        num1=int(input("enter a number:"))
         num2=answer
         
     if(op1=="*"):
@@ -19,11 +19,28 @@ while b=="c":
     elif(op1=="+"):
         answer=num1+num2
     elif(op1=="-"):
-        answer=num2-num2
-    elif(op1=="/"):
-        answer=num2/num1
+        if counter==0:
+            answer=num1-num2
+        else:
+            answer=num2-num1
+    elif(op1=="/"):        
+        if counter==0:
+            if num2==0:
+                print("invalid input ")
+                continue
+            answer=num1/num2
+        else:
+            if num1==0:
+                print("invalid input ")
+                continue
+            answer=num2/num1
         
-    print(num2,op1,num1,"=", answer)
+        
+    print("answer = ", answer)
+    counter+=1
+    b=input("enter break or continue: ")
+    if b=="b":
+        break
     counter+=1
     b=input("enter break or continue: ")
     if b=="b":
